27a7ce7c6144c6de0ec0687510ac1a54a40a09f4
WsilvaM <wadst25@gmail.com>
2021-11-03 21:44:11 -0300

    Calculadora

---
+++ flutter_application_1/lib/homeapp.dart
@@ -0,0 +1,15 @@
+class HomeApp extends StatefulWidget {
+  const HomeApp({ Key? key }) : super(key: key);
+
+  @override
+  _HomeAppState createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
